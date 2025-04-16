d4b3bf8b7e51191aa565010d52203a59aa61150b
Jacob (Gyeongju) Kim <abox00@naver.com>
2025-04-16 09:56:39 +0900

    나의 목표 페이지 커밋

--- SetMyGoal.swift
+++ SetMyGoal.swift
@@ -8,25 +8,29 @@
 import SwiftUI
 
 struct SetMyGoal: View {
+    //목표를 저장할 변수
+    @State private var goal: String = ""
+    
     var body: some View {
-//        @State private var Goal: string = ""
-        //목표를 저장할 변수
-        
         VStack  {
             Spacer()
-            Text("나의 목표")
-            TextField("나의 목표를 작성해봐요", text: .constant(""))
-                .frame(width: 361, height: 84)
-                //왜 width는 적용이 안되지..?
-            //TextField에 SF Symbol 넣는 방법은??? (text 타입이라..)
-                .font(.PretendardBold24)
-                .foregroundColor(.blackSecondary)
-                .padding(.horizontal, 40.0)
-                .background(Color.material01)
-                .multilineTextAlignment(.center)
-                //textfield의 Text 중앙정렬
+            ZStack {
+                    TextField("나의 목표를 작성해봐요", text: $goal)
+                        .padding()
+                        .frame(width: 361, height: 84)
+                        //왜 width는 적용이 안되지..?
+                        //TextField에 SF Symbol 넣는 방법은??? (text 타입이라..)
+                        .font(.PretendardBold24)
+                        .foregroundColor(.blackSecondary)
+                        .padding(.horizontal, 40.0)
+                        .background(Color.material01)
+                        .multilineTextAlignment(.center)
+                        //textfield의 Text 중앙정렬
+                Image(systemName: "pencil")
+                //ZStack으로 SF Symbol을 앞 Depth에 배치 (TextFeild에는 Text만)
+            }
             Spacer()
-            Button{
+            Button {
             } label: {
                 Text("다음")
                     .font(.PretendardBold24)
@@ -37,8 +41,7 @@ struct SetMyGoal: View {
     }
 }
 
-
-
 #Preview {
     SetMyGoal()
 }
+
